c67759d4e54081270cf2aa8a56bf931ee886732e
Mariia Lavreniuk <mariia.lavreniuk@oneclicklca.com>
2026-04-14 08:37:00 +0200

    Part 1.6 Intoduced Unicafe feedback buttons

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,45 @@
+import Statistics from "./Statistics";
+import Feedback from "./Feedback";
+import { useState } from 'react'
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  let texts = {
+    goodTxt: 'good',
+    neutralTxt: 'neutral',
+    badTxt: 'bad'
+  };
+
+  let variables = {
+    good: good,
+    neutral: neutral,
+    bad: bad
+  }
+
+  let feedback = {
+    header: 'give feedback',
+    setGood,
+    setNeutral,
+    setBad,
+    variables: variables,
+    texts: texts
+  };
+
+  let statistics = {
+    header: 'statistics',
+    texts: texts,
+    variables: variables
+  };
+
+  return (
+    <>
+      <Feedback feedback={feedback} />
+      <Statistics statistics={statistics} />
+    </>
+  )
+}
+
+export default App;
---
+++ part1/unicafe/src/Statistics.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+
+const Statistics = ({ statistics }) => {
+  const texts = statistics.texts
+  const variables = statistics.variables
+
+  return (
+    <>
+      <Header header={statistics.header} />
+      <div>
+        <p>{texts.goodTxt}: {variables.good}</p>
+        <p>{texts.neutralTxt}: {variables.neutral}</p>
+        <p>{texts.badTxt}: {variables.bad}</p>
+      </div>
+    </>
+  )
+}
+
+export default Statistics;
