5d54bff4bd6f6634e4e2af6910506ed9d1d22d66
Mariia Lavreniuk <155968002+mariia-lavreniuk-ocl@users.noreply.github.com>
2026-09-02 12:23:54 +0200

    Refactored the code according to the part 1.8 (#15)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,39 @@
-import Statistics from "./Statistics";
+import Header from "./Header";
 import Feedback from "./Feedback";
 import { useState } from 'react'
+
+const calculateAverage = (goodScores, badScores, allScoresNum) => {
+  let result = (goodScores - badScores) / allScoresNum;
+  return result || 0;
+}
+
+const calculateAll = (goodScores, neutralScores, badScores) => {
+  return goodScores + neutralScores + badScores;
+}
+
+const calculatePositive = (positiveScores, allScores) => {
+  return allScores === 0 ? 0 : (positiveScores * 100 / allScores);
+}
+
+const Statistics = ({ statistics }) => {
+  const texts = statistics.texts
+  const variables = statistics.variables
+  const allScores = calculateAll(variables.good, variables.neutral, variables.bad);
+
+  return (
+    <>
+      <Header header={statistics.header} />
+      <div>
+        <p>{texts.goodTxt}: {variables.good}</p>
+        <p>{texts.neutralTxt}: {variables.neutral}</p>
+        <p>{texts.badTxt}: {variables.bad}</p>
+        <p>{texts.allTxt}: {allScores}</p>
+        <p>{texts.averageTxt}: {calculateAverage(variables.good, variables.bad, allScores)}</p>
+        <p>{texts.positiveTxt}: {calculatePositive(variables.good, allScores)}</p>
+      </div>
+    </>
+  )
+}
 
 function App() {
   const [good, setGood] = useState(0)
